cb37cd4c86f1cc1ccc48150067de439ff276aa00
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2024-02-23 10:55:55 -0800

    my b ad

---
+++ ScriptingProjectP3Jureidini/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    // Start is called before the first frame update
+    //float result = Mathf.Lerp (3f, 5f, 0.5f);
+    //the result is 4, so the light intensity is aswell
+
+    //an alternative way to achieve this
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+    //Here result (4, 5, 6)
+    Vector3 result = Vector3. Lerp (from, to, 0.75f);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lighting per second instead of per frame
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+        //change Lerp to SmoothDamp for most situations unless you reallllly want Lerp
+    }*/
+}
